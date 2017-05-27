c0fd23ff9d37ac20c41a621ec4d345810ae3e562
Daniel Aparicio <daparicio52@gmail.com>
2017-05-27 16:41:10 -0400

    Cliente Rest

---
+++ src/main/java/RestClient.java
@@ -0,0 +1,79 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+
+public class RestClient {
+    public static void main(String[] args) {
+        try {
+
+            while(true){
+
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+
+                HttpPost postRequest = new HttpPost(
+                        "http://localhost:8080/stepLog");
+
+                ArrayList<NameValuePair> postParameters;
+
+                postParameters = new ArrayList<NameValuePair>();
+                Random rand = new Random();
+
+                Integer  n = rand.nextInt(50) + 1;
+                postParameters.add(new BasicNameValuePair("steps",n.toString()));
+                postRequest.setEntity(new UrlEncodedFormEntity(postParameters));
+
+                postRequest.addHeader("accept", "application/json");
+
+                HttpResponse response = httpClient.execute(postRequest);
+
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP error code : "
+                            + response.getStatusLine().getStatusCode());
+                }
+
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader((response.getEntity().getContent())));
+
+                String output;
+                System.out.println("Output from Server .... \n");
+                while ((output = br.readLine()) != null) {
+                    System.out.println(output);
+                }
+                httpClient.getConnectionManager().shutdown();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+
+
+        } catch (ClientProtocolException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
